7b2b1d9493dde9b5d9bf0af681c6e3543cf0cb7d
Yassine1000L <yassneyas11@gmail.com>
2026-06-02 15:45:39 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', function () {
-    return 'Hello World';
+    return 'Hello Worldddddddddddddddddddddddddddddddddddddddddddddddddddddddd';
 });
 
 Route::permanentRedirect('/', '/hello');
