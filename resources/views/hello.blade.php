796ec377a2407f1f471a90953c177054bc7925b2
NTrang81 <your-email@example.com>
2026-06-03 15:19:32 +0200

    naam update hello.blade

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,6 +1,6 @@
 <x-site-layout>
     <div class="container text-center hero-box">
         <h1 class="page-title" style="font-size:3rem; margin-bottom:0.5rem;">Aquafin</h1>
-        <p class="page-subtitle" style="font-size:1.25rem;">Assia · Niels · Trang · Yassine</p>
+        <p class="page-subtitle" style="font-size:1.25rem;">Assia · Niels · Trang · Thien Y · Yassine</p>
     </div>
 </x-site-layout>
