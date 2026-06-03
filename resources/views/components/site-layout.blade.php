1c047f0f35a0710a069fd37bfc662ab7e4fd57f4
Yassine1000L <yassneyas11@gmail.com>
2026-06-03 13:59:18 +0200

    Watergolf decoratie onder navigatiebalk

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -10,6 +10,10 @@
 
     @include('layouts.app_navigation')
 
+    <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 80" preserveAspectRatio="none">
+        <path fill="#2563eb" fill-opacity="0.08" d="M0,40 C360,80 720,0 1080,40 C1260,60 1350,50 1440,40 L1440,80 L0,80 Z"/>
+    </svg>
+
     <main>
         {{ $slot }}
     </main>
