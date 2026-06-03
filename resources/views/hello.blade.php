2ad7c2b71f4161f83c34d1e6ae24ad31cccee8f1
Yassine1000L <yassneyas11@gmail.com>
2026-06-03 14:53:09 +0200

    refactor homepage

--- resources/views/hello.blade.php
+++
@@ -1,6 +0,0 @@
-<x-site-layout>
-    <div class="container text-center hero-box">
-        <h1 class="page-title" style="font-size:3rem; margin-bottom:0.5rem;">Aquafin</h1>
-        <p class="page-subtitle" style="font-size:1.25rem;">Assia · Niels · Thien · Trang · Yassine</p>
-    </div>
-</x-site-layout>
