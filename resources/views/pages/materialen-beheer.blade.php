e5d80f192d312e79fd1cd1069450d7a323cec3eb
Yassine1000L <yassneyas11@gmail.com>
2026-06-04 12:07:05 +0200

    nav link beheer

--- resources/views/pages/materialen-beheer.blade.php
+++ resources/views/pages/materialen-beheer.blade.php
@@ -1,11 +1,13 @@
 <x-site-layout>
 
-    <a href="{{ route('materialen.create') }}">
-    + Nieuw materiaal
-    </a>
-    
     <div class="container">
         <h1 class="page-title">Materiaal beheren</h1>
+
+        <div style="margin-bottom: 20px;">
+            <a href="{{ route('materialen.create') }}" class="btn" style="background:#2563eb;color:#fff;padding:10px 24px;border-radius:8px;font-size:15px;">
+            + Nieuw materiaal
+            </a>
+        </div>
 
         <table class="custom-table">
             <thead>
