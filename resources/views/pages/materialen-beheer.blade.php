86b31b31b3aa28a7bea41f611ee2505c1c975314
assjhr <assia.jouhri11@gmail.com>
2026-06-04 12:17:27 +0200

    Materiaalbeheer uitgebreid met wijzigen en verwijderen

--- resources/views/pages/materialen-beheer.blade.php
+++ resources/views/pages/materialen-beheer.blade.php
@@ -24,7 +24,9 @@
                         <td>{{ $materiaal->subcategorie->naam ?? 'N/A' }}</td>
                         <td>{{ $materiaal->beschrijving }}</td>
                         <td>
-                            <button>Wijzigen</button>
+                            <a href="{{ route('materialen.edit', $materiaal) }}">
+                                Wijzigen
+                            </a>
                             <form method="POST" action="{{ route('materialen.destroy', $materiaal) }}" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
