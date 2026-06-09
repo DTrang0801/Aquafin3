5127c2836b7b242979a29052acf19fdc1fc12e82
NTrang81 <your-email@example.com>
2026-06-09 12:24:35 +0200

    Commentaar StockDashboardController

--- app/Http/Controllers/StockDashboardController.php
+++ app/Http/Controllers/StockDashboardController.php
@@ -6,13 +6,14 @@ use App\Models\Materiaal;
 use Illuminate\View\View;
 
 class StockDashboardController extends Controller
-{
+{ // Toont de stock dashboard pagina met de 20 meest bestelde materialen
+
     public function index(): View
     {
         $mostOrderedItems = Materiaal::query()
-            ->with('subcategorie.categorie')
-            ->where('order_count', '>', 0)
-            ->orderByDesc('order_count')
+            ->with('subcategorie.categorie') 
+            ->where('order_count', '>', 0) // Materialen die minstens 1 keer besteld zijn
+            ->orderByDesc('order_count') // Sorteer van meest naar minst besteld
             ->limit(20)
             ->get();
 
