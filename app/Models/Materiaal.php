a7c6afb07edbdfab64ec6d89fa30a98551b6a010
NielsTanghe1 <nielsiepielsie1510@gmail.com>
2026-06-15 14:15:41 +0200

    fix

--- app/Models/Materiaal.php
+++ app/Models/Materiaal.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\NullableFloodRiskLevelCast;
 use App\Enums\FloodRiskLevel;
 use Database\Factories\MateriaalFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
