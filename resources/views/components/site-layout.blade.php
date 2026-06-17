c95d641e7e2b3387f35c45ae066e2a45dd512bef
ThienY99 <nguyenthieny@hotmail.com>
2026-06-17 15:14:06 +0200

    feat: add dark mode persistence to site layout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aquafin</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script>
+        if (localStorage.getItem('darkMode') === 'true') {
+            document.documentElement.classList.add('dark');
+        }
+    </script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.1/dist/cdn.min.js"></script>
 </head>
 <body>
